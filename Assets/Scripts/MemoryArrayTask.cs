e026d3dee045a78427261950bc4b21c874604de2
AndreiMarks <hey@ndrei.us>
2015-01-04 21:47:26 -0500

    First Commit

---
+++ Assets/Scripts/MemoryArrayTask.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryArrayTask : MonoBehaviour 
+{
+	void Start () 
+    {
+        StartCoroutine( BeginMemoryTask() );	
+	}
+    
+    private IEnumerator BeginMemoryTask()
+    {
+        yield return 0;
+    }
+}
